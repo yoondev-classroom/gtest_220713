40e00269f32819c83928eeeaf65f43d94b472a10
Chansik Yun <chansigi@gmail.com>
2022-07-13 22:59:26 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -5,42 +5,51 @@
 
 #include <gtest/gtest.h>
 
-TEST(SampleTest, First) {}
-TEST(SampleTest, Second) {}
+// - 테스트케이스 단위 픽스쳐 - xUnit / 신선한 픽스쳐 전략
+// - 테스트스위트 단위 픽스쳐 - xUnit / 공유 픽스쳐 전략
+// - 프로그램 단위 픽스쳐    - Google Test
 
-TEST(ImageTest, First) {}
-TEST(ImageTest, Second) {}
+TEST(SampleTest, First)
+{
+}
+TEST(SampleTest, Second)
+{
+}
 
+TEST(ImageTest, First)
+{
+}
+TEST(ImageTest, Second)
+{
+}
 
 // 전역 픽스쳐를 사용하는 방법
 // 1. testing::Environment 를 상속받아서 SetUp()/TearDown()을
 //    정의합니다.
-class MyTestEnvironment : public testing::Environment
-{
+class MyTestEnvironment : public testing::Environment {
 public:
-  void SetUp() override
-  {
-    printf("MyTestEnvironment.SetUp()\n");
-  }
+    void SetUp() override
+    {
+        printf("MyTestEnvironment.SetUp()\n");
+    }
 
-  void TearDown() override
-  {
-    printf("MyTestEnvironment.TearDown()\n");
-  }
+    void TearDown() override
+    {
+        printf("MyTestEnvironment.TearDown()\n");
+    }
 };
 
-class MyTestEnvironment2 : public testing::Environment
-{
+class MyTestEnvironment2 : public testing::Environment {
 public:
-  void SetUp() override
-  {
-    printf("MyTestEnvironment.SetUp2()\n");
-  }
+    void SetUp() override
+    {
+        printf("MyTestEnvironment.SetUp2()\n");
+    }
 
-  void TearDown() override
-  {
-    printf("MyTestEnvironment.TearDown2()\n");
-  }
+    void TearDown() override
+    {
+        printf("MyTestEnvironment.TearDown2()\n");
+    }
 };
 
 // 2. TestEnvironment를 설치하는 방법
@@ -48,15 +57,15 @@ public:
 #if 1
 int main(int argc, char** argv)
 {
-  // 1. 구글 테스트 라이브러리 초기화
-  testing::InitGoogleTest(&argc, argv);
+    // 1. 구글 테스트 라이브러리 초기화
+    testing::InitGoogleTest(&argc, argv);
 
-  // 반드시 new를 통해 생성해야 합니다.
-  testing::AddGlobalTestEnvironment(new MyTestEnvironment);
-  testing::AddGlobalTestEnvironment(new MyTestEnvironment2);
+    // 반드시 new를 통해 생성해야 합니다.
+    testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+    testing::AddGlobalTestEnvironment(new MyTestEnvironment2);
 
-  // 2. 모든 테스트를 실행하고, 테스트 결과를 리포트합니다.
-  return RUN_ALL_TESTS();
+    // 2. 모든 테스트를 실행하고, 테스트 결과를 리포트합니다.
+    return RUN_ALL_TESTS();
 }
 #endif
 
@@ -65,4 +74,3 @@ int main(int argc, char** argv)
 #if 0
 testing::Environment* env1 = testing::AddGlobalTestEnvironment(new MyTestEnvironment);
 #endif
-
