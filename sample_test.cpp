4a7beca798ce5b7ecf1ca2aabf95ca21f5f46ef5
Chansik Yun <chansigi@gmail.com>
2022-07-13 02:25:25 +0000

    .

---
+++ sample_test.cpp
@@ -0,0 +1,15 @@
+// sample_test.cpp
+
+// Google Test를 사용할 때 포함하는 헤더파일입니다.
+#include <gtest/gtest.h>
+
+// 테스트 케이스 방법
+// : TEST(Test Suite Name, Test Case Name)
+
+TEST(SampleTest, FirstTest)
+{
+}
+
+TEST(SampleTest, SecondTest)
+{
+}
