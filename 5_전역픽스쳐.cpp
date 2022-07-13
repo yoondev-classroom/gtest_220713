634aa553bab6050d56f2f1772acb7bd834da2dc3
Chansik Yun <chansigi@gmail.com>
2022-07-13 07:45:37 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -29,10 +29,23 @@ public:
   }
 };
 
-// 2. TestEnvironment를 설치하는 방법
-//  1) main: 사용자가 main을 직접 정의하고 있을 때
-//  2) 전역 변수: 사용자가 main을 정의하고 있지 않을 때
+class MyTestEnvironment2 : public testing::Environment
+{
+public:
+  void SetUp() override
+  {
+    printf("MyTestEnvironment.SetUp2()\n");
+  }
 
+  void TearDown() override
+  {
+    printf("MyTestEnvironment.TearDown2()\n");
+  }
+};
+
+// 2. TestEnvironment를 설치하는 방법
+//  1) main: 사용자가 main을 직접 정의하고 있을 때 => 권장
+#if 1
 int main(int argc, char** argv)
 {
   // 1. 구글 테스트 라이브러리 초기화
@@ -40,7 +53,16 @@ int main(int argc, char** argv)
 
   // 반드시 new를 통해 생성해야 합니다.
   testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+  testing::AddGlobalTestEnvironment(new MyTestEnvironment2);
 
   // 2. 모든 테스트를 실행하고, 테스트 결과를 리포트합니다.
   return RUN_ALL_TESTS();
 }
+#endif
+
+//  2) 전역 변수: 사용자가 main을 정의하고 있지 않을 때
+//  > 전역 변수의 초기화가 main 이전에 수행되는 특징을 이용합니다.
+#if 0
+testing::Environment* env1 = testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+#endif
+
