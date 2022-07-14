384dac56dce39eafcfd86914c5dbf9fa6cd42f7d
Chansik Yun <chansigi@gmail.com>
2022-07-14 02:45:28 +0000

    .

--- 9_테스트전용하위클래스.cpp
+++ 9_테스트전용하위클래스.cpp
@@ -11,12 +11,13 @@ public:
 
 #include <gtest/gtest.h>
 
-// SUT가 제공하는 확인해야 하는 상태가 protected 이면
 // => 테스트 전용 하위 클래스(Test Specific Subclass Pattern)
 //   의도: SUT가 클래스에 테스트를 위한 기능을 제공하지 않을 때,
 //        SUT의 하위 클래스를 만들어서 테스트되지 않은 요구사항을 검증할 수 있습니다.
 //   장점: 제품코드를 변경하지 않고, 테스트할 수 있습니다.
-
+//  1) SUT가 제공하는 확인해야 하는 상태가 protected 일 때
+//  2) SUT가 테스트를 위한 기능을 제공하고 있지 않을 때
+//      - C++ 제약: 가상 함수
 class TestUser : public User {
 public:
     using User::GetAge; // protected -> public
