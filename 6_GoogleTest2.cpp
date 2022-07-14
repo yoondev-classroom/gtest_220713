cfa1fae3f323923460860e32248158ac8cd25acb
Chansik Yun <chansigi@gmail.com>
2022-07-14 01:08:18 +0000

    .

--- 6_GoogleTest2.cpp
+++ 6_GoogleTest2.cpp
@@ -21,3 +21,20 @@ TEST(ImageTest, hoo) { }
 TEST(ImageProcessorTest, foo) { }
 TEST(ImageProcessorTest, goo) { }
 TEST(ImageProcessorTest, hoo) { }
+
+int cnt;
+int GetCount() { return ++cnt; }
+
+// 7. 테스트 반복 / 테스트 순서 무작위
+// $ ./a.out --gtest_repeat=100 --gtest_shuffle --gtest_break_on_failure
+//  > "변덕스러운 테스트"를 확인하는 목적으로 사용할 수 있습니다.
+TEST(SampleTest, Sample1)
+{
+    EXPECT_EQ(GetCount(), 1);
+}
+
+TEST(SampleTest, Sample2)
+{
+    EXPECT_EQ(GetCount(), 2);
+    cnt = 0;
+}
