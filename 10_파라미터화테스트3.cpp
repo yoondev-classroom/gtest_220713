23d296a8ea1bc3f82d2635c12ad176fef34a314c
Chansik Yun <chansigi@gmail.com>
2022-07-14 04:44:51 +0000

    .

--- 10_파라미터화테스트3.cpp
+++ 10_파라미터화테스트3.cpp
@@ -22,13 +22,45 @@ INSTANTIATE_TEST_SUITE_P(ProcessValues, ProcessTest,
     testing::Values(
         InputData { 10, "Tom", true },
         InputData { 5, "Bob", true },
-        InputData { 3, "Alice", false },
+        InputData { 3, "Alice", true }, // !!
         InputData { 4, "Bobby", false },
         InputData { 10, "David", true }));
+
+// 사용자 정의 타입은 구글 테스트 프레임워크에 의해서 제대로 출력되지 않습니다.
+std::ostream& operator<<(std::ostream& os, const InputData& data)
+{
+    return os << "Input{arg1=" << data.arg1 << ", name=" << data.name << "}";
+}
 
 TEST_P(ProcessTest, Sample1)
 {
     const InputData& data = GetParam();
 
     EXPECT_EQ(data.expected, Process(data.arg1, data.name));
+}
+
+//--------------------
+// std::tuple을 이용하면 여러개의 타입을 하나의 타입으로 묶을 수 있습니다.
+using InputDataType2 = std::tuple<int, std::string, bool>;
+
+class ProcessTest2 : public testing::TestWithParam<InputDataType2> {
+};
+
+INSTANTIATE_TEST_SUITE_P(ProcessValues2, ProcessTest2,
+    testing::Values(
+        InputDataType2{ 10, "Tom", true },
+        InputDataType2{ 5, "Bob", true },
+        InputDataType2{ 3, "Alice", true }, // !!
+        InputDataType2{ 4, "Bobby", false },
+        InputDataType2{ 10, "David", true }));
+
+TEST_P(ProcessTest2, Sample1)
+{
+    const InputDataType2& data = GetParam();
+    
+    int arg1 = std::get<0>(data);
+    std::string name = std::get<1>(data);
+    bool expected = std::get<2>(data);
+      
+    EXPECT_EQ(expected, Process(arg1, name));
 }
