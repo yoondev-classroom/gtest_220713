bd62b13afc687e3a3494a371d3182589ae3c5800
Chansik Yun <chansigi@gmail.com>
2022-07-15 05:46:32 +0000

    .

--- 20_동작검증2.cpp
+++ 20_동작검증2.cpp
@@ -24,27 +24,55 @@ public:
 
 void Process(Dog* p)
 {
-  p->Second();
-  p->Third();
-  p->Fourth();
-  p->First();
+    p->First();
+    p->Second();
+    p->Third();
+    p->Fourth();
 }
 
 // First -> Second -> Third -> Fourth
 // - 호출 순서 검증
 //   testing::InSequence
-
 using testing::InSequence;
 
 TEST(DogTest, Process1)
 {
-  InSequence seq; // !!!!
-  MockDog mock;
+    InSequence seq; // !!!!
+    MockDog mock;
 
-  EXPECT_CALL(mock, First);
-  EXPECT_CALL(mock, Second);
-  EXPECT_CALL(mock, Third);
-  EXPECT_CALL(mock, Fourth);
+    EXPECT_CALL(mock, First);
+    EXPECT_CALL(mock, Second);
+    EXPECT_CALL(mock, Third);
+    EXPECT_CALL(mock, Fourth);
 
-  Process(&mock);
+    Process(&mock);
+}
+
+void Process2(Dog* p)
+{
+    p->First();
+
+    p->Third();
+
+    p->Second();
+
+    p->Fourth();
+}
+
+// First ------> Second          : 1
+//       |
+//       ------> Third -> Fourth : 2
+using testing::Sequence;
+
+TEST(DogTest, Process2)
+{
+    MockDog mock;
+    Sequence seq1, seq2;
+
+    EXPECT_CALL(mock, First).InSequence(seq1, seq2);
+    EXPECT_CALL(mock, Second).InSequence(seq1);
+    EXPECT_CALL(mock, Third).InSequence(seq2);
+    EXPECT_CALL(mock, Fourth).InSequence(seq2);
+
+    Process2(&mock);
 }
